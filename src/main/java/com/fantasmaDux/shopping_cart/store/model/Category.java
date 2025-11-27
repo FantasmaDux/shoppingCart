507fb48ae8774595c7601813ac51d2c3e13db559
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-11-27 14:31:14 +0300

    add entities, postgresql connection

---
+++ src/main/java/com/fantasmaDux/shopping_cart/store/model/Category.java
@@ -0,0 +1,25 @@
+package com.fantasmaDux.shopping_cart.store.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
---
+++ src/main/java/com/fantasmaDux/shopping_cart/store/model/Product.java
@@ -0,0 +1,39 @@
+package com.fantasmaDux.shopping_cart.store.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String brand;
+    private BigDecimal price; // Точность операций
+    private int inventory; // Количество на складе, чтобы не путать с заказанным количеством
+    private String description;
+
+    // Категория и продукт не зависят друг от друга
+    // Пишем all, так как это отношение должно удалиться при удалении
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    // когда продукт будет удален, все связанные с ним image тоже будут удалены
+    // CascadeType.REMOVE удаляет дочерние entity только когда удаляют родительскую
+    // orphanRemoval = true удаляет дочерние entity когда их убирают из коллекции
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+}
